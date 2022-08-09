cf0ecbdb92fd2f26c61a01a8d6fd6f95ba3d1fc2
Robert Taylor <rvtaylor94@gmail.com>
2022-08-08 21:37:16 -0600

    added login functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import Login from './Login.jsx';
 import axios from 'axios';
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState('');
 
-  const getUser = (user) => {
+  const getUser = (user, callback) => {
     axios.get('/users', {
       params: user
     })
-      .then((data) => {
-        console.log(data);
+      .then((results) => {
+        console.log(results.data);
+        callback(results.data);
       })
       .catch((err) => { console.error(err) })
   }
 
   return (
     <div>
-      <h1 className='title'>Journal</h1>
-      <Login getUser={getUser} />
+      <h1 className='title'>{currentUser ? `${currentUser.username}'s Journal` : 'Journal'}</h1>
+      <Login getUser={getUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,36 +1,71 @@
 import React from "react";
 import { useState } from "react";
 
-const Login = ({ getUser }) => {
+const Login = ({ getUser, currentUser, setCurrentUser }) => {
   const [loginClicked, setLoginClicked] = useState(false);
-  const [loginUserName, setLoginUserName] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
+  const [loginUserName, setLoginUserName] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [invalid, setInvalid] = useState(false);
 
   const handleSubmit = () => {
     let user = {
       username: loginUserName,
-      password: loginPassword
+      password: loginPassword,
     };
-    getUser(user);
+    setLoginUserName("");
+    setLoginPassword("");
 
+    getUser(user, (userArray) => {
+      if (userArray.length === 0) {
+        setInvalid(true);
+      } else {
+        setInvalid(false);
+        setCurrentUser(userArray[0]);
+        setLoginClicked(false);
+      }
+    });
+  };
+
+  const handleSignOut = () => {
+    if (confirm('Are you sure you want to sign out?')) {
+      setCurrentUser('');
+    }
   }
 
   return (
     <div>
-      <div className="login">
-        <button onClick={() => setLoginClicked(!loginClicked)}>Log In</button>
-        {loginClicked ?
+      {currentUser ? (
+        <button onClick={() => handleSignOut()}>Sign Out</button>
+      ) : (
         <div>
-          <input value={loginUserName} placeholder="Username" onChange={(e) => setLoginUserName(e.target.value)} />
-          <input type="password" value={loginPassword} placeholder="Password" onChange={(e) => setLoginPassword(e.target.value)} />
-          <button onClick={() => handleSubmit()}>Submit</button>
-        </div>
-        : null}
-      </div>
+          <div className="login">
+            <button onClick={() => setLoginClicked(!loginClicked)}>
+              Log In
+            </button>
+            {loginClicked ? (
+              <div>
+                {invalid ? <div>Invalid username/password!</div> : null}
+                <input
+                  value={loginUserName}
+                  placeholder="Username"
+                  onChange={(e) => setLoginUserName(e.target.value)}
+                />
+                <input
+                  type="password"
+                  value={loginPassword}
+                  placeholder="Password"
+                  onChange={(e) => setLoginPassword(e.target.value)}
+                />
+                <button onClick={() => handleSubmit()}>Submit</button>
+              </div>
+            ) : null}
+          </div>
 
-      <div className="signup">
-        <button>Sign Up</button>
-      </div>
+          <div className="signup">
+            <button>Sign Up</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
